8045afb183e12b6a17bce7f7cad8e3e88042749e
a-sanches <andressasanchesadvocacia@gmail.com>
2022-07-28 15:54:52 -0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+
+let semana = ["Segunda", "Terça", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"] 
+let date = new Date();
+document.getElementById("dia-atual").innerHTML = semana[d.getDay()];
+
+function mes() {
+  
+  
+}
