c85bbef4dcf4ec49b4c3a789b71d144548fe6716
a-sanches <andressasanchesadvocacia@gmail.com>
2022-08-08 13:59:08 -0300

    atualizacao

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,5 @@
 
-let semana = ["Segunda", "Terça", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"] 
-let date = new Date();
-document.getElementById("dia-atual").innerHTML = semana[d.getDay()];
 
-function mes() {
-  
-  
+for (let i = 0; i < days.length; i++) {
+  return <h1></h1>
 }
